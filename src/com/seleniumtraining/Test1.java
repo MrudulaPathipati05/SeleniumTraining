ae3ca3baf5ee5e0dcd8cbf156810d4f216c9bae7
usertaco <usertaco@users-air.lan>
2023-06-08 23:04:24 -0400

    Day3 work

---
+++ src/com/seleniumtraining/Test1.java
@@ -0,0 +1,9 @@
+package com.seleniumtraining;
+
+public class Test1 {
+	//getting main method - main ctrl+space
+	//sysout,ctrl+space
+public static void main(String...args) {
+	System.out.println("Fisrt sample class");
+}
+}
