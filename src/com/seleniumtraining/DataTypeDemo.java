f9256a987149b5dcc5b2dd2ad7280fdd32ed669d
usertaco <usertaco@users-air.lan>
2023-06-12 23:18:51 -0400

    added data type demo class file

---
+++ src/com/seleniumtraining/DataTypeDemo.java
@@ -0,0 +1,16 @@
+package com.seleniumtraining;
+
+public class DataTypeDemo {
+	public static void main(String[] args) {
+		byte b1 =(byte) 300;
+				System.out.println(b1);
+		
+	
+	int i = 020;
+	System.out.println(i);
+	
+	short sh = (short)32768;
+	System.out.println(sh);
+	}
+
+}
